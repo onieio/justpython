c8bdc5a8eb877bd12d62f021a1077a1528b6f644
onieio <raj1351945@gmail.com>
2023-02-20 16:41:04 +0000

    Added the function and test case

--- devopslib/randomfruit.py
+++ devopslib/randomfruit.py
@@ -4,3 +4,11 @@ from random import choices
 def fruit():
     fruits = ["apple", "cherry", "pear"]
     return choices(fruits)[0]
+
+def meal(beverage):
+    my_fruit = fruit()
+    if my_fruit == "cherry":
+        complete_meal = f"Your meal is a {my_fruit} and {beverage}"
+        return complete_meal
+    alternate_meal = f"Your meal is rice and {beverage}"
+    return alternate_meal
